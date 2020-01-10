518bb1954cea6c26daacf2f1f0b9edec296f1b24
Srinivas Kasarla <Srinivas Kasarla@Lenovo-PC>
2020-01-10 15:08:13 +0530

    working set of spring rest and spring boot and spring jpa

--- SpringMVCBoot-1/src/main/java/com/home/test/SpringMvcBoot1Application.java
+++ SpringMVCBoot-1/src/main/java/com/home/test/SpringMvcBoot1Application.java
@@ -3,8 +3,6 @@ package com.home.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com")
 @EnableJpaRepositories("com.home.dao")
 @EntityScan("com.home.bean")
-@EnableEurekaServer
 public class SpringMvcBoot1Application {
 
 	public static void main(String[] args) {
